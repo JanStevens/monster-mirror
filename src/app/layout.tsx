5cf91a8271b63b964243ef8a0379c5d84cf64469
Jan Stevens <jan.stevens@novemberfive.co>
2023-11-30 15:17:00 +0100

    feat: add splash screen with logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,59 @@ export const metadata: Metadata = {
   appleWebApp: {
     title: 'Monster Mirror',
     statusBarStyle: 'black',
-    startupImage: '/icon.png',
+    // Generated using: https://appsco.pe/developer/splash-screens
+    startupImage: [
+      {
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+        url: '/splash/iphone5_splash.png',
+      },
+      {
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+        url: '/splash/iphone6_splash.png',
+      },
+      {
+        media:
+          '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
+        url: '/splash/iphoneplus_splash.png',
+      },
+      {
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+        url: '/splash/iphonex_splash.png',
+      },
+      {
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+        url: '/splash/iphonexr_splash.png',
+      },
+      {
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+        url: '/splash/iphonexsmax_splash.png',
+      },
+      {
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
+        url: '/splash/ipad_splash.png',
+      },
+      {
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)',
+        url: '/splash/ipadpro1_splash.png',
+      },
+      {
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)',
+        url: '/splash/ipadpro3_splash.png',
+      },
+      {
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
+        url: '/splash/ipadpro2_splash.png',
+      },
+    ],
     capable: true,
   },
   formatDetection: {
