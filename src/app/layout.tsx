980360f2a20af625668220eccc0eaae186989cee
Jan Stevens <jan.stevens@novemberfive.co>
2024-05-02 15:12:44 +0200

    fix: add features

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -230,7 +230,10 @@ export default function RootLayout({
       dir="ltr"
       className={`dark ${PirataOneGloomhaven.variable} ${PhilosopherBold.variable}`}
     >
-      <Script src="//cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js" />
+      <Script
+        strategy="beforeInteractive"
+        src="//cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces2020%2Ces2021%2Ces2022"
+      />
       <body>
         <PWALifeCycle />
         <Flex
