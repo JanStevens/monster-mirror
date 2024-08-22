5a41cae6b4b53ca3d3cd1b9d046886812c75bc53
Jan Stevens <jan.stevens@novemberfive.co>
2024-08-22 15:47:46 +0200

    fix: add version in appbar

--- src/components/@navigation/MainNavigation.tsx
+++ src/components/@navigation/MainNavigation.tsx
@@ -1,8 +1,16 @@
+import { Link } from 'components/@common/link';
+
 import Navigation from './Navigation';
 
 const MainNavigation = () => (
   <Navigation>
     <Navigation.Logo title="Monster Mirror" />
+    <Link
+      color="fg.subtle"
+      href={`https://github.com/JanStevens/monster-mirror/releases/tag/${process.env.NEXT_PUBLIC_APP_VERSION}`}
+    >
+      v{process.env.NEXT_PUBLIC_APP_VERSION}
+    </Link>
   </Navigation>
 );
 
